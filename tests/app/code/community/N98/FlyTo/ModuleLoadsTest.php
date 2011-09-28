f5b2d893f66aa3c6a502681e436257491a591730
Alexander Menk <a.menk@netz98.de>
2011-09-28 08:54:54 +0200

    Test added

---
+++ tests/app/code/community/N98/FlyTo/ModuleLoadsTest.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * netz98 FlyTo magento module
+ *
+ * LICENSE
+ *
+ * This source file is subject of netz98.
+ * You may be not allowed to change the sources
+ * without authorization of netz98 new media GmbH.
+ *
+ * @copyright Copyright (c) 2011 netz98 new media GmbH (http://www.netz98.de)
+ * @author netz98 new media GmbH <info@netz98.de>
+ * @category N98
+ * @package N98_Social
+ */
+
+class N98_FlyTo_ModuleLoadsTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * Tests if the block is correctly loaded
+     */
+    public function testLoadBlock()
+    {
+        $block = Mage::app()->getLayout()->createBlock('n98flyto/flyer');
+
+        //validate instance
+        $this->assertInstanceOf('N98_FlyTo_Block_Flyer', $block);
+    }
+}
